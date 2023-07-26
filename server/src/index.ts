750860c980e231cd2f8cdfb45dc9aa91cfd349c4
nada-abuzaid <nada.b.abu.zaid@gmail.com>
2023-07-26 14:28:46 +0300

    feat: add get avatars route

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,27 @@
 import express from 'express';
 import cors from 'cors';
+import axios from 'axios';
 
 const app = express();
 require('dotenv').config();
 
 app.use(cors());
 app.use(express.json());
+
+app.get('/api/multiavatar', async (req, res) => {
+  const apiUrl = `https://api.multiavatar.com/4645646/${Math.round(
+    Math.random() * 1000
+  )}?apikey=DAa6teh0jXM4lL`;
+  const imageResponse = await axios(apiUrl, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'image/svg+xml',
+    },
+  });
+
+  res.setHeader('Content-Type', 'image/svg+xml');
+  res.send(imageResponse.data);
+});
 
 const server = app.listen(process.env.PORT, () =>
   console.log(`Server started on ${process.env.PORT}`)
